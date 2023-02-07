ea47b26b11ba4e77d6cc41205e2a78e35b468dbd
Maqsud Ahmad <106242051+MaqLA@users.noreply.github.com>
2023-02-07 15:41:26 +0800

    Repair port handling

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.get("/search", (req, res)=>{
     res.render("search");
 });
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Express server running on localhost:3000");
 });
